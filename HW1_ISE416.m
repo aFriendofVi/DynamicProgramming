3423d6b8e52d9097896c8b127ebdf0a74d55b942
Jerry Wong <jerrywong41@gmail.com>
2016-09-04 22:21:59 -0400

    1st homework

--- HW1_ISE416.m
+++ HW1_ISE416.m
@@ -1,14 +1,44 @@
-threshold = 100; % initial mean value of the offers
-days = 30; % horizon
-meanDayValue = 0; % policy value of the last day is exactly the initial threshold
-for i = days:-1:1 % backfold from the last day to the first day
-    sim = normrnd(100,20,100000,1); % generate simulation data
-    policy = sim>=threshold; % policy function (binary)
-    actionValue = max(sim,threshold); % evaluate the action;
-    threshold = mean(actionValue); % update the threshold
-    meanDayValue = [threshold,meanDayValue]; % expected reward at the beginning of day_i;
+%=================================================================
+% Title: ISE-416 Dynamic Programming
+%           Homework 1
+% Auther: Yuheng Huang (Jerry)
+% Sept 2, 2016 at 18:00 EST
+% Platform: Window 7
+% IDE: Matlab R2015b
+%=================================================================
+
+
+% initial mean value of the offers is the initial threshold
+threshold = 100; 
+
+% Set horizon
+days = 30; 
+
+% Expected value of the last day is exactly the initial threshold
+meanDayValue = 0; 
+
+% backfold from the last day to the first day
+for i = days:-1:1 
+    % generate 100000 simulation data
+    sim = normrnd(100,20,100000,1); 
+    
+    % Action (binary)
+    action = sim>=threshold; 
+    
+    % evaluate the action;
+    actionValue = max(sim,threshold); 
+    
+    % update the threshold
+    threshold = mean(actionValue); 
+    
+    % Store the expected reward at the beginning of day_i in a vector;
+    meanDayValue = [threshold,meanDayValue]; 
 end
+
+% Artist painting behind the screen
 hold on;
 plot(meanDayValue);
 title(['Expected reward at day 1: ',num2str(meanDayValue(1))]);
 xlabel('Day');ylabel('Threshold');axis([1,31,0,meanDayValue(1)+20]);
+clc;
+disp(['Expected reward at day 1: ',num2str(meanDayValue(1)),' dollars']);
